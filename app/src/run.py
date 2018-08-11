20231f79db169f2e37b5cf30143540f5c13d6f03
Taishi Asato <asatai@TaishinoMacBook-puro.local>
2018-08-11 21:19:26 +0900

    更新

--- app/src/run.py
+++ app/src/run.py
@@ -1,3 +1,4 @@
+import pymysql
 from flask import Flask, request, render_template, redirect, make_response, url_for
 
 application = Flask(__name__)
@@ -5,12 +6,52 @@ application = Flask(__name__)
 @application.route('/')
 def test():
 
+    test = 'test'
+
+    db = pymysql.connect(host='172.19.0.1', user='root', password='asatai951156', db='docker', charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
+    con = db.cursor()
+    print('???')
+
+    sql = 'select id, test from test where id = 2'
+    con.execute(sql)
+    db.commit()
+    print('test')
+
+    result = con.fetchall()
+    print(result)
+
+    return render_template('index.html', data=result)
+
+@application.route('/', methods=['POST'])
+def top_test():
+
+    test_text = request.form['test']
+    print(test_text)
+
+    db = pymysql.connect(host='172.19.0.1', user='root', password='asatai951156', db='docker', charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
+    con = db.cursor()
+    print('???')
+
+    sql = 'insert into test(test) values(%s) '
+    con.execute(sql, [test_text])
+    db.commit()
+
+    result_test = con.fetchall()
+    print(result_test)
+
     return render_template('index.html')
 
 @application.route('/test')
 def db():
 
-    return "やっとできたー"
+    test = 'docker'
+
+    resp = make_response(render_template('test.html', test_sub='testだよ'))
+    test_resp = resp.set_cookie("name" ,test)
+    print(resp)
+    print(test_resp)
+
+    return resp
 
 @application.route('/app')
 def test_sub():
